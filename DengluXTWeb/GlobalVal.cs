e94535cf52520f81d9b0c6f5e1fa603e917a04a1
jiaolong <zzia615@163.com>
2020-12-04 22:34:38 +0800

    立项

---
+++ DengluXTWeb/GlobalVal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DengluXTWeb
+{
+    public class GlobalVal
+    {
+        public const string ConStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dengluXtWeb;Integrated Security=true";
+    }
+}
---
+++ DengluXTWeb/表结构.sql
@@ -0,0 +1,19 @@
+﻿create database dengluXtWeb
+go
+
+use dengluXtWeb
+go
+
+create table denglu
+(
+code nvarchar(20) not null primary key,
+password nvarchar(100) not null,
+name nvarchar(20) not null,
+sex nvarchar(10) not null,
+birthday datetime not null
+)
+go
+
+insert into denglu values('admin','123456','admin',N'男','2000-01-01')
+go
+
